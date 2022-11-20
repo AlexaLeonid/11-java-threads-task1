92babf58e6fa4c37b8e2dfd4fa5a71da3f0e2f20
Leonidova Alexandra <alexandraleonid2002@gmail.ru>
2022-11-20 16:18:58 +0500

    Module11.1 Леонидова

--- 11-java-threads-task1/src/Lucky.java
+++ 11-java-threads-task1/src/Lucky.java
@@ -1,16 +1,18 @@
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Lucky {
-    static int x = 0;
-    static int count = 0;
+    static AtomicInteger atX = new AtomicInteger(0);
+    static AtomicInteger count = new AtomicInteger(0);
 
     static class LuckyThread extends Thread {
         @Override
         public void run() {
-            while (x < 999999) {
-                x++;
+            while (atX.get() < 999999) {
+                int x = atX.incrementAndGet();
                 if ((x % 10) + (x / 10) % 10 + (x / 100) % 10 == (x / 1000)
                         % 10 + (x / 10000) % 10 + (x / 100000) % 10) {
                     System.out.println(x);
-                    count++;
+                    count.incrementAndGet();
                 }
             }
         }
